fb41ff2e7b8f403252cb027976e9838db5f37c8c
Catalysa <amaryllis.scann@gmail.com>
2017-02-23 20:42:52 +0000

    sdsd

--- reading_file.h
+++ reading_file.h
@@ -1,1 +1,15 @@
-int ReadingFile();
+struct ret
+{
+	int nbVideo;
+	int nbEndpoint;
+	int nbRequest;
+	int nbCache;
+	int capacityCache;
+	double** latencyCache;
+	double* latencyData;
+	double** request;
+};
+typedef struct ret ret;
+
+
+ret *ReadingFile();
