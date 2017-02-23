d5d3239dff2a5ca76c7c2466300d581c19fcc1cd
Catalysa <amaryllis.scann@gmail.com>
2017-02-23 18:10:05 +0000

    reading_file.c

--- reading_file.c
+++ reading_file.c
@@ -4,11 +4,11 @@
 
 struct general
 {
-   int nbVideo;
-   int nbEndpoint;
-   int nbRequest;
-   int nbCache;
-   int capacityCache;
+   char nbVideo[50];
+   char nbEndpoint[50];
+   char nbRequest[50];
+   char nbCache[50];
+   char capacityCache[50];
 };
 typedef struct general;
 
@@ -19,11 +19,52 @@ typedef enum { False, True } Boolean;
 
 int ReadingFile(char * file_name)
 {
+	general param;
+	int check = 0;
+	int indexStore = 0;
+	current = fgetc(file);
 	// check si le fichier s'ouvre correctement
 	FILE *file = fopen(file_name, "r");
 	if(file == NULL)
 	{
 		printf("ERREUR - le fichier ne s'ouvre pas correctement\n\n");
+	}
+
+	while(current != '\n')
+	{
+		if(current == ' ')
+		{
+			check = check + 1;
+			indexStore = 0;
+			continue;
+		}
+		if(check == 0)
+		{
+			param.nbVideo[indexStore] = current; 
+			indexStore = indexStore + 1;
+		}
+		if(check == 1)
+		{
+			param.nbEndpoint[indexStore] = current; 
+			indexStore = indexStore + 1;
+		}
+		if(check == 2)
+		{
+			param.nbRequest[indexStore] = current; 
+			indexStore = indexStore + 1;
+		}
+		if(check == 3)
+		{
+			param.nbCache[indexStore] = current; 
+			indexStore = indexStore + 1;
+		}
+		if(check == 4)
+		{
+			param.capacityCache[indexStore] = current; 
+			indexStore = indexStore + 1;
+		}
+		current = fgetc(file);
+		
 	}
 
 	int current = fgetc(file);
@@ -42,4 +83,5 @@ int ReadingFile(char * file_name)
 
 int main (int argc, char *argv[])
 {
+	
 }
